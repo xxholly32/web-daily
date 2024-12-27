d90ee7e4d48186451e0961c502995a6ec2b72c2a
xxholly32 <xxholly32@163.com>
2024-12-27 09:08:19 +0800

    feat: add  visitor

---
+++ docs/.vitepress/theme/Layout.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import DefaultTheme from 'vitepress/theme'
+import Analysis from './components/Analysis.vue'
+
+const { Layout } = DefaultTheme
+</script>
+
+<template>
+  <Layout>
+    <template #nav-bar-content-before>
+      <Analysis />
+    </template>
+  </Layout>
+</template>
---
+++ docs/.vitepress/theme/components/Analysis.vue
@@ -0,0 +1,8 @@
+<template>
+  <div style="padding-left: 16px">
+    <img
+      src="https://badges.pufler.dev/visits/xxholly32/xxholly32-web-daily"
+      alt="Visits Badge"
+    >
+  </div>
+</template>
